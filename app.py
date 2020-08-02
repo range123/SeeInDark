a3e33ca09284a94a873d03e9e97e957b0a604bb8
Jayaraman <adarsh1999@gmail.com>
2020-08-03 02:47:49 +0530

    Moved network to different file and refactored code

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-#/mnt/c/Users/aashi/Desktop/Flaskdev/flaskenv/bin/env/python3.7
 from flask import Flask,render_template,session,redirect,url_for,request
-#from flask_wtf import FlaskForm
-#from wtforms import StringField, TextField, SubmitField
-#from __future__ import print_function, division
 import os
 import torch
 import pandas as pd
 import requests
 import cv2
-#from skimage import io, transform
 import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import numpy as np
 torch.set_printoptions(linewidth=120)
-#import cv2
-# Ignore warnings
 import json
 import warnings
 warnings.filterwarnings("ignore")
@@ -27,7 +18,7 @@ import time
 from flask import make_response
 from functools import wraps, update_wrapper
 from datetime import datetime
-
+from network import NNetwork
 plt.ion()   # interactive mode
 
 app = Flask(__name__)
@@ -44,38 +35,11 @@ def nocache(view):
         
     return update_wrapper(no_cache, view)
 
-class NNetwork(nn.Module):
-    def __init__(self):
-        super(NNetwork,self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=3,out_channels=4,kernel_size=3,padding=0)#16
-        self.conv2 = nn.Conv2d(in_channels=4,out_channels=8,kernel_size=3,padding=0)
-        
-        self.deconv1 = nn.ConvTranspose2d(in_channels=8,out_channels=4,kernel_size=4)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=4,out_channels=3,kernel_size=4)
-        
-    
-    def forward(self,t):
-        t = self.conv1(t)
-        t = F.relu(t)
-        t = F.max_pool2d(t,kernel_size=3, stride = 1)
-        
-        t = self.conv2(t)
-        t = F.relu(t)
-        
-        t = self.deconv1(t)
-        t = F.relu(t)
 
-        t = self.deconv2(t)
-        t = F.relu(t)
-
-        print("yoyo:",t.shape)
-        
-        return t
 
 
 fcn = NNetwork().to('cpu')
-fcn.load_state_dict(torch.load('./tiny_final.pt',map_location= torch.device('cpu')))
+fcn.load_state_dict(torch.load('./models/tiny_final.pt',map_location= torch.device('cpu')))
 
 @app.route('/test', methods=['GET'])
 @nocache
@@ -115,7 +79,6 @@ def index():
 
 	cv2.imwrite("./static/"+timeval+"1.jpg",out_pr)
 
-	# plt.imshow(out_pr.astype('int'))
 	return render_template("result.html",data={"ori":"./static/"+timeval+".jpg","pro":"./static/"+timeval+"1.jpg"})
 
 
@@ -134,6 +97,6 @@ def add_header(r):
 
 	
 
-#if __name__=='__main__':
-app.run("0.0.0.0",5001,debug=True)
+if __name__=='__main__':
+    app.run("0.0.0.0",5001,debug=True)
 
